04c4530df9c6726936dcf63e0e5f17af6c72516f
NamAnh76 <119741583+NamAnh76@users.noreply.github.com>
2023-06-24 18:27:15 +0700

    Add files via upload

---
+++ jsi-demo02/Signin/js.js
@@ -0,0 +1,50 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuL-CyR0vWvZRTkFwD_dhq0yq8iXnw3iU",
+  authDomain: "project-jsi18.firebaseapp.com",
+  projectId: "project-jsi18",
+  storageBucket: "project-jsi18.appspot.com",
+  messagingSenderId: "537043743152",
+  appId: "1:537043743152:web:daa2cfc6770ee1c0716675",
+  measurementId: "G-SK5FN4PYXP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const Email = document.getElementById("Email")
+const Password = document.getElementById("Password")
+
+
+
+
+document.getElementById("Signin").onclick = () =>{
+signInWithEmailAndPassword(auth, Email.value, Password.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(userCredential.user,"Dang nhap thanh cong")
+    alert("Đăng nhập thành công")
+    window.location.replace("../index2.html")
+    document.getElementById("title01").innerHTML = Email.value
+    // ...
+    
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(error.message,"Dang nhap that bai")
+    alert("Đăng nhập thất bại")
+  });
+}
+
+
+    
+
+
+
